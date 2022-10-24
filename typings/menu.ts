080109db56b751af08e95b3ca7507d2710d9f493
Jenny Poulson <911us@duck.com>
2022-10-24 20:28:19 +0000

    Click from other people prevention.

--- typings/menu.ts
+++ typings/menu.ts
@@ -31,24 +31,29 @@ export const show_schedule = (day: string) => {
     return message;
 }
 
-export const schedule_days_menu = new Menu("schedule_days_menu", { onMenuOutdated: "Updated, try now.", autoAnswer: false })
+export const schedule_days_menu = new Menu("schedule_days_menu", { onMenuOutdated: "Updated, try now." })
     .text("Понеділок", (ctx) => {
+        if (ctx.update.message?.from?.id != ctx.from.id) return ctx.answerCallbackQuery("❌ Ви не можете використовувати цю кнопку!");
         let s = show_schedule("Monday");
         ctx.editMessageText(s, { parse_mode: "Markdown", disable_web_page_preview: true })
     })
     .text("Вівторок", async (ctx) => {
+        if (ctx.update.message?.from?.id != ctx.from.id) return ctx.answerCallbackQuery("❌ Ви не можете використовувати цю кнопку!");
         let s = show_schedule("Tuesday");
         ctx.editMessageText(s, { parse_mode: "Markdown", disable_web_page_preview: true })
     })
     .text("Середа", async (ctx) => {
+        if (ctx.update.message?.from?.id != ctx.from.id) return ctx.answerCallbackQuery("❌ Ви не можете використовувати цю кнопку!");
         let s = show_schedule("Wednesday");
         ctx.editMessageText(s, { parse_mode: "Markdown", disable_web_page_preview: true })
     }).row()
     .text("Четвер", async (ctx) => {
+        if (ctx.update.message?.from?.id != ctx.from.id) return ctx.answerCallbackQuery("❌ Ви не можете використовувати цю кнопку!");
         let s = show_schedule("Thursday");
         ctx.editMessageText(s, { parse_mode: "Markdown", disable_web_page_preview: true })
     })
     .text("П'ятниця", async (ctx) => {
+        if (ctx.update.message?.from?.id != ctx.from.id) return ctx.answerCallbackQuery("❌ Ви не можете використовувати цю кнопку!");
         let s = show_schedule("Friday");
         ctx.editMessageText(s, { parse_mode: "Markdown", disable_web_page_preview: true })
     })
